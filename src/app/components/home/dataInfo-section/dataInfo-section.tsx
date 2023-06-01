95db57dac0ba45be0882ceb32cf58f88c4642a33
Andriy Nosov <nosovandriy@gmail.com>
2023-06-01 23:11:05 +0300

    feature/dataInfo-section add desktop layout

--- src/app/components/home/dataInfo-section/dataInfo-section.tsx
+++ src/app/components/home/dataInfo-section/dataInfo-section.tsx
@@ -1,32 +1,57 @@
+"use client";
+
 import Image from "next/image";
+import CountUp from "react-countup";
 
 import style from "./dataInfo-section.module.scss";
 
 const DataInfo = () => {
   return (
-    <section>
+    <section className={style.dataInfo}>
       <div className={style.data}>
-        <div>
-          <h4>567</h4>
-          <p>PAINTINGS IN DATABASE</p>
+        <div className={style.info}>
+          <h4 className={style.quantity}>
+            <CountUp start={0} end={45} duration={2} decimals={0} />
+          </h4>
+          <p className={style.text}>ARTISTS IN COMMUNITY</p>
         </div>
-        <div>
-          <h4>1000000 €</h4>
-          <p>RAISED FUNDS</p>
+        <div className={style.info}>
+          <h4 className={style.quantity}>
+            <CountUp start={0} end={567} duration={3} decimals={0} />
+          </h4>
+          <p className={style.text}>PAINTINGS IN DATABASE</p>
         </div>
-        <div>
-          <h4>45</h4>
-          <p>ARTISTS IN COMMUNITY</p>
+        <div className={style.info}>
+          <h4 className={style.quantity}>
+            <CountUp
+              start={0}
+              end={1000000}
+              duration={4}
+              decimals={0}
+              suffix=" €"
+            />
+          </h4>
+          <p className={style.text}>RAISED FUNDS</p>
         </div>
       </div>
-      <div className={style.ornament}>
-        <Image
-          src="/assets/dataInfo.webp"
-          alt="Ukrainian gallery ornament"
-          width={1000}
-          height={1000}
-          className={style.image}
-        />
+      <div className={style.wrapper}>
+        <button className={style.button}>Learn more</button>
+        <div className={style.ornament}>
+          <Image
+            src="/assets/dataInfoMob.webp"
+            alt="Ukrainian gallery ornament"
+            width={1000}
+            height={1000}
+            className={style.imageMob}
+          />
+          <Image
+            src="/assets/dataInfoDesk.webp"
+            alt="Ukrainian gallery ornament"
+            width={1000}
+            height={1000}
+            className={style.imageDesk}
+          />
+        </div>
       </div>
     </section>
   );
