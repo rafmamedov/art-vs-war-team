24fd9fcd0ecf57e6f68ae59495319e4781641591
Andriy Nosov <nosovandriy@gmail.com>
2023-06-03 22:31:26 +0300

    feature/funds-section rename home components

---
+++ src/app/components/home/artist-section/artist-section.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import Carousel from './carousel/carousel';
+
+import styles from './artist-section.module.scss';
+
+const Artist = () => {
+  return (
+    <section className={styles.artist}>
+      <div className={styles.content}>
+        <div className={styles.background}>
+          <Carousel />
+        </div>
+
+        <h2 className={styles.title}>Are you an artist?</h2>
+        <h6 className={styles.subtitle}>Become a part of our conscious community</h6>
+        <div className={styles.button}>Create account</div>
+      </div>
+    </section>
+  );
+};
+
+export default Artist;
