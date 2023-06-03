b09cef6f1019092cc11639cc93e11abb81840419
Andriy Nosov <nosovandriy@gmail.com>
2023-06-03 22:20:23 +0300

    feature-section fix merge conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import About from "@/app/components/home/about-section/about-section";
 import Funds from "@home/funds-section/funds-section";
 import DataInfo from "@home/dataInfo-section/dataInfo-section";
-import HaveCollection from "@home/have collection/have collection";
-import Artist from "@/app/components/home/artist/artist-section";
+import HaveCollection from "@/app/components/home/collection-section/collection-section";
+import Artist from "@home/artist-section/artist-section";
 
 export default function Home() {
   return (
